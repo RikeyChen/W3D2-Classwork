d588931e0000fe3ab8ce44d90aeb3f5e7d8f7072
rikeychen <rikey.chen16@gmail.com>
2018-11-06 11:54:37 -0800

    Add some methods for Question, Reply, and User classes

--- aa_questions.rb
+++ aa_questions.rb
@@ -26,6 +26,18 @@ class Question
     Question.new(question.first)
   end
 
+  def self.find_by_author_id(user_id)
+    question = QuestionsDBConnection.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        user_id = ?
+    SQL
+    Question.new(question.first)
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -33,7 +45,79 @@ class Question
     @user_id = options['user_id']
   end
 
+
 end
+
+
+class Reply
+  attr_accessor :question_id, :parent_id, :user_id, :body
+
+  def self.find_by_user_id(user_id)
+    replies = QuestionsDBConnection.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+    replies.map { |reply_data| Reply.new(reply_data) }
+  end
+
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDBConnection.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+    SQL
+    replies.map { |reply_data| Reply.new(reply_data) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @body = options['body']
+    @user_id = options['user_id']
+    @parent_id = options['parent_id']
+  end
+
+end
+
+class User
+  attr_accessor :fname, :lname
+
+  def self.find_by_name(fname, lname)
+    users = QuestionsDBConnection.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND lname = ?
+    SQL
+    users.map { |user_data| User.new(user_data) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def authored_questions
+    Question.find_by_author_id(@id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
+
+
+end
+
 
 
 #   def self.all
